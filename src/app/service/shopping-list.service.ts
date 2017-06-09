bdfccccc147c0dd9ceaa8765cb43475449ddbe40
oleg.bezkorovaynyi <oleg.bezkorovaynyi@gmail.com>
2017-06-09 13:22:44 +0300

    loading shopping list items to the form

--- src/app/service/shopping-list.service.ts
+++ src/app/service/shopping-list.service.ts
@@ -3,6 +3,7 @@ import {Subject} from 'rxjs/Subject';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -11,6 +12,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngreident(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {

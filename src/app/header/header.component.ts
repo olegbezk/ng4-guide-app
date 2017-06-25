50554d8df4cf77ed52ce7faddd5980f016eea22b
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-06-25 16:55:42 +0300

    getting back recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,4 +18,8 @@ export class HeaderComponent {
         }
     );
   }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
--- src/app/service/data-storage.service.ts
+++ src/app/service/data-storage.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import { RecipeService } from './recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -11,5 +12,14 @@ export class DataStorageService {
 
     storeRecipes() {
         return this.http.put(this.BASE_URL, this.recipeService.getRecipes());
+    }
+    
+    getRecipes() {
+        this.http.get(this.BASE_URL).subscribe(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                this.recipeService.setRecipes(recipes);
+            }
+        );
     }
 }
